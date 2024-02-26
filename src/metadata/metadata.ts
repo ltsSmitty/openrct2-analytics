6d185278f12d8798432e94e0045f42bce5038db2
Smitty Penman <smitty.penman@gmail.com>
2024-02-26 13:41:10 -0500

    implement metadata

---
+++ src/metadata/metadata.ts
@@ -0,0 +1,35 @@
+import { pluginVersion } from "../config";
+import { v4 as uuidv4 } from "uuid";
+
+function getMetadata() {
+  // todo id
+  return {
+    context: {
+      park: {
+        scenario: {
+          fileName: scenario.filename,
+          scenarioName: scenario.name,
+          name: park.name,
+        },
+        inGameDate: {
+          year: date.year,
+          month: date.month,
+          day: date.day,
+          ticksElapsed: date.ticksElapsed,
+          monthProgress: date.monthProgress,
+        },
+      },
+      library: {
+        pluginVersion,
+        apiVersion: context.apiVersion,
+      },
+      network: {
+        networkMode: network.mode,
+        // add potentially other network info, like player id, etc.
+      },
+      // language could be nice, but isn't exposed
+    },
+    messageID: uuidv4(),
+    timestamp: new Date().toISOString(),
+  };
+}
