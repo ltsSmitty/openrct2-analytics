5c8ea800cad9837fae57c2af70b039c4c4d67318
Smitty Penman <smitty.penman@gmail.com>
2024-03-19 06:44:49 -0400

    add other hooks

---
+++ src/hooks/other.ts
@@ -0,0 +1,16 @@
+export const onOtherAction = <T extends OtherAction>(
+  otherAction: T,
+  callback: TCallback
+) => {
+  switch (otherAction) {
+    default:
+      return context.subscribe(
+        "action.execute",
+        (data: GameActionEventArgs<object>) => {
+          if (data.action === otherAction && (data.args as any).flags <= 0) {
+            callback(data);
+          }
+        }
+      );
+  }
+};
